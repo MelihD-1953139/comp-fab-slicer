6ebf19108b5a08bef2deaad27ddaa675a2bf94df
KobeW-1952857 <kobe.werner@student.uhasselt.be>
2024-10-22 10:02:23 +0200

    Added Nexus

---
+++ premake5.lua
@@ -0,0 +1,62 @@
+workspace "Slicer"
+	configurations { "Debug", "Release" }
+	architecture "x86_64"
+
+	outputdir = "%{cfg.system}/%{cfg.buildcfg}"
+	targetdir ("%{wks.location}/bin/" .. outputdir)
+	objdir ("%{wks.location}/obj/" .. outputdir)
+
+	startproject "Slicer"
+
+	filter "system:macosx"
+		links { "Cocoa.framework", "OpenGL.framework", "IOKit.framework", "CoreVideo.framework", "QuartzCore.framework" }
+		
+	filter""
+
+	include "vendor/Nexus"
+	include "vendor/Nexus/vendor/premake5_glfw.lua"
+	include "vendor/Nexus/vendor/premake5_ImGui.lua"
+	include "vendor/Nexus/vendor/Glad"
+
+project "Slicer"
+	kind "ConsoleApp"
+	language "C++"
+	cppdialect "C++20"
+	staticruntime "on"
+
+	includedirs {
+		"src",
+		"include",
+		"vendor",
+		"vendor/Nexus/include",
+		"vendor/Nexus/vendor",
+		"vendor/Nexus/vendor/Glad/include",
+		"vendor/Nexus/vendor/GLFW/include",
+		"vendor/Nexus/vendor/glm/glm",
+		"vendor/Nexus/vendor/ImGui",
+		"vendor/Nexus/vendor/spdlog/include",
+	}
+
+	files { 
+		"include/**.h",
+		"src/**.cpp",
+		"vendor/Nexus/vendor/glm/glm/**.hpp",
+		"vendor/Nexus/vendor/glm/glm/**.inl",
+	}
+
+	links {
+		"Nexus",
+		"glfw",
+		"Glad",
+		"ImGui"
+	}
+	
+	filter "configurations:Debug"
+		defines { "DEBUG" }
+		symbols "On"
+	
+	filter "configurations:Release"
+		defines { "NDEBUG" }
+		optimize "On"
+	
+	filter ""
