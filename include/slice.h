90733c72f8c2764d4d3b8f87fb87cef4eed360b9
KobeW-1952857 <kobe.werner@student.uhasselt.be>
2024-11-18 10:23:56 +0100

    Fixed .obj resources

--- include/slice.h
+++ include/slice.h
@@ -10,45 +10,9 @@
 
 struct Line {
   glm::vec3 p1, p2;
-
-  void setNextPoint(glm::vec3 point) {
-    if (firstPointSet) {
-      p2 = point;
-    } else {
-      p1 = point;
-      firstPointSet = true;
-    }
-  }
-
-  void sort(glm::vec3 referencePoint) {
-    double angle1 =
-        glm::atan(-(p1.z - referencePoint.z), p1.x - referencePoint.x);
-    double angle2 =
-        glm::atan(-(p2.z - referencePoint.z), p2.x - referencePoint.x);
-
-    bool crossingXAxis = (p1.z > referencePoint.z && p2.z < referencePoint.z) ||
-                         (p1.z < referencePoint.z && p2.z > referencePoint.z);
-
-    auto calculateXaxisCrossing = [](glm::vec3 p1, glm::vec3 p2,
-                                     glm::vec3 referencePoint) {
-      return p1.x + (referencePoint.z - p1.z) * (p2.x - p1.x) / (p2.z - p1.z);
-    };
-
-    if (crossingXAxis &&
-        calculateXaxisCrossing(p1, p2, referencePoint) < referencePoint.x) {
-      std::swap(p1, p2);
-      std::swap(angle1, angle2);
-    } else if (angle2 > angle1) {
-      std::swap(p1, p2);
-      std::swap(angle1, angle2);
-    }
-  }
-
-  Line operator*(glm::vec3 scale) {
-    p1 *= scale;
-    p2 *= scale;
-    return *this;
-  }
+  void setNextPoint(glm::vec3 point);
+  void sort(glm::vec3 referencePoint);
+  Line operator*(glm::vec3 scale);
 
 private:
   bool firstPointSet = false;
--- src/slice.cpp
+++ src/slice.cpp
@@ -1,9 +1,46 @@
 #include "slice.h"
 
-#define GLM_ENABLE_EXPERIMENTAL
 #include <Nexus.h>
+#include <clipper2/clipper.h>
 
-#include <glm/gtx/string_cast.hpp>
+void Line::setNextPoint(glm::vec3 point) {
+  if (firstPointSet) {
+    p2 = point;
+  } else {
+    p1 = point;
+    firstPointSet = true;
+  }
+}
+
+void Line::sort(glm::vec3 referencePoint) {
+  double angle1 =
+      glm::atan(-(p1.z - referencePoint.z), p1.x - referencePoint.x);
+  double angle2 =
+      glm::atan(-(p2.z - referencePoint.z), p2.x - referencePoint.x);
+
+  bool crossingXAxis = (p1.z > referencePoint.z && p2.z < referencePoint.z) ||
+                       (p1.z < referencePoint.z && p2.z > referencePoint.z);
+
+  auto calculateXaxisCrossing = [](glm::vec3 p1, glm::vec3 p2,
+                                   glm::vec3 referencePoint) {
+    return p1.x + (referencePoint.z - p1.z) * (p2.x - p1.x) / (p2.z - p1.z);
+  };
+
+  if (crossingXAxis &&
+      calculateXaxisCrossing(p1, p2, referencePoint) < referencePoint.x) {
+    std::swap(p1, p2);
+    std::swap(angle1, angle2);
+  } else if (angle2 > angle1) {
+    std::swap(p1, p2);
+    std::swap(angle1, angle2);
+  }
+}
+
+Line Line::operator*(glm::vec3 scale) {
+  p1 *= scale;
+  p2 *= scale;
+  return *this;
+}
 
 Contour::Contour(std::vector<glm::vec3> points) : m_points(points) {
   initOpenGLBuffers();
